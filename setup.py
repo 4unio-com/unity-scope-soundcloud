e3b0481288416b274ab1082bf55a11d0b2676aa2
David Callé <davidc@framli.eu>
2013-02-13 15:31:48 +0100

    Generate packaging

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from DistUtilsExtra.command import *
+
+setup(name='unity-scope-soundcloud',
+      version='0.1',
+      author='David Callé',
+      author_email='davidc@framli.eu',
+      url='http://launchpad.net/ubuntu-scopes',
+      license='GNU General Public License (GPL)',
+      data_files=[
+    ('share/dbus-1/services', ['data/unity-scope-soundcloud.service']),
+    ('share/icons/unity-icon-theme/places/svg', ['data/icons/service-soundcloud.svg']),
+    ('lib/ubuntu-scopes/soundcloud', ['src/unity_soundcloud_daemon.py']),
+    ('lib/ubuntu-scopes/soundcloud', ['src/__init__.py']),
+    ], cmdclass={'build':  build_extra.build_extra,
+                 'build_i18n': build_i18n.build_i18n,})
