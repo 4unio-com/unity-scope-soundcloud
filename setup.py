027deee956e7bb57b236c33f448ae406bcc0fdf1
David Callé <davidc@framli.eu>
2013-03-20 21:25:24 +0100

    Don't install the service icon

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ setup(name='unity-scope-soundcloud',
       license='GNU General Public License (GPL)',
       data_files=[
     ('share/dbus-1/services', ['data/unity-scope-soundcloud.service']),
-    ('share/icons/unity-icon-theme/places/svg', ['data/icons/service-soundcloud.svg']),
     ('share/unity-scopes/soundcloud', ['src/unity_soundcloud_daemon.py']),
     ('share/unity-scopes/soundcloud', ['src/__init__.py']),
     ], cmdclass={'build':  build_extra.build_extra,
